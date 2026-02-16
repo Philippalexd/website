8fa6cea50607f6f6d510e88fe118583d1f2c884a
Philippalexd <philipp2502@gmail.com>
2026-02-16 22:55:41 +0100

    Changed url

--- assets/supabase.js
+++ assets/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://XXXX.supabase.co";
+const SUPABASE_URL = "https://odpbvvjuvwdjwhkjpykm.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_5OHJxuFD8QHQsK9YeWSMjQ_ruq7Lhrk";
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
