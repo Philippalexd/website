4eb45ebb3812ef97d6ffe961947a223c2cd60053
Philipp Alexander Dreis <philipp.dreis@uni-due.de>
2026-04-17 22:04:02 +0200

    had two times profile provider. Now only inside app

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
-import { ProfileProvider } from "./context/ProfileContext.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ProfileProvider>
-        <App />
-      </ProfileProvider>
+      <App />
     </QueryClientProvider>
   </StrictMode>,
 );
